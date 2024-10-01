7f3830483fa67960c0741bc59f89b18c9c930717
mckechniep@gmail.com <mckechniep@MagicalBox.>
2024-10-01 12:22:05 -0400

    added application homepage

--- models/user.js
+++ models/user.js
@@ -1,6 +1,31 @@
+// models/user.js
+
+// models/user.js
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,6 +34,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema], // embedding the applicationSchema here
 });
 
 const User = mongoose.model('User', userSchema);
