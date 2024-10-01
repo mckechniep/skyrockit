a8be79a967d72e66421489d9f734b4aee658ed01
mckechniep@gmail.com <mckechniep@MagicalBox.>
2024-10-01 14:21:09 -0400

    fixed controller routes

---
+++ controllers/applications.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+router.get('/', (req, res) => {
+  try {
+    res.render('applications/index.ejs');
+  } catch (error) {
+    console.error(error);
+    res.redirect('/')
+  }
+});
+
+router.get('/new', async (req, res) => {
+  res.render('applications/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+module.exports = router;
